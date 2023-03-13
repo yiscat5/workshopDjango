4c41fce5f4af2ba8bf88b85bcea13fddafaa403f
Yiscat55 <ycarretero8@gmail.com>
2023-03-12 22:13:02 -0500

    Inicio 0.02

--- workShop/settings.py
+++ workShop/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'workShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'workShop.wsgi.application'
 
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'workShop/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
