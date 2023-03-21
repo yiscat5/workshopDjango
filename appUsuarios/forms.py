a6103f0d972b99c2cd31b0af3d28135580fff13d
Yiscat55 <ycarretero8@gmail.com>
2023-03-21 13:51:38 -0500

    Actualizacion workshop copiado

---
+++ appUsuarios/forms.py
@@ -0,0 +1,14 @@
+from django import forms 
+from .models import Usuario 
+
+class UsuarioForm(forms.ModelForm): 
+     class Meta: 
+          model = Usuario 
+          fields = ['first_name', 'last_name', 'username', 'email']
+          labels = {
+               'first_name': 'Nombre: ', 
+               'last_name': 'Apellido: ', 
+               'email': 'Correo E: ', 
+               'password': 'Contraseña: ', 
+          }
+          
--- appUsuarios/views.py
+++ appUsuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Usuario 
 from django.contrib import auth 
 from django.contrib.auth.decorators import login_required 
+from .forms import UsuarioForm
+
 
 def login(request): 
      if request.method == 'POST': 
@@ -24,3 +26,29 @@ def login(request):
 def logout(request): 
      auth.logout(request) 
      return redirect('login')
+
+def registrar(request): 
+     if request.method == 'POST': 
+          form = UsuarioForm(request.POST) 
+          if form.is_valid(): 
+               first_name = form.cleaned_data['first_name'] 
+               last_name = form.cleaned_data['last_name'] 
+               email = form.cleaned_data['email'] 
+               password = form.cleaned_data['password'] 
+               
+               username = email.split('@')[0] 
+               
+               existe = Usuario.objects.filter(email=email).exists()
+               
+               if not existe: 
+                    user = Usuario.objects.create_user(first_name=first_name, last_name=last_name, 
+username=username, email=email, password=password) 
+                    user.is_active = True 
+                    user.save() 
+                    return render(request, 'usuarios/login.html') 
+          else: 
+               return render(request, 'usuarios/registro.html', {'form': form}) 
+     
+     else: 
+          form = UsuarioForm() 
+          return render(request, 'usuarios/registro.html', {'form': form})
